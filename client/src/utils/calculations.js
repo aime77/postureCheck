8d79e9a93310c501a55179135ec0ec753bfbb663
Jeme <aime.urquieta@outlook.com>
2019-01-24 15:00:25 -0600

    function to calculate score

---
+++ client/src/utils/calculations.js
@@ -0,0 +1,15 @@
+export const calculatePoints=(data,a,b,c,d,keyScore)=>{
+
+const position=data.position;
+let pointsTotal=0;
+if(position.x<a & position.x>b & position.y < c & position.y>d){
+const pointsTotalArray=[];
+pointsTotalArray.push(data.score);
+pointsTotalArray.forEach(val=>{
+    pointsTotal=+val;
+})
+}
+
+if (pointsTotal===keyScore) return true;
+
+}
